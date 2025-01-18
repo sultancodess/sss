e5e0c93dad0b057d5f163e7f39e341daf46a66b6
Sultan Alam <sultancodess@gmail.com>
2025-01-18 23:05:09 +0530

    Update p.js
    just for test

--- css/p.js
+++ css/p.js
@@ -6,7 +6,7 @@ function checkEvenOdd(number) {
         console.log(number + " is odd.");
     }
 }
-
+console.log ("heelo");
 // Example usage:
 checkEvenOdd(10); // Output: 10 is even.
 checkEvenOdd(7);  // Output: 7 is odd.
