76b90bcfdd1f22a310e3f8167e3e6e70d1936fd1
sultan alam <sultancodess@gmail.com>
2025-01-18 23:01:36 +0530

    hello bro

---
+++ css/p.js
@@ -0,0 +1,12 @@
+// Basic JavaScript
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        console.log(number + " is even.");
+    } else {
+        console.log(number + " is odd.");
+    }
+}
+
+// Example usage:
+checkEvenOdd(10); // Output: 10 is even.
+checkEvenOdd(7);  // Output: 7 is odd.
